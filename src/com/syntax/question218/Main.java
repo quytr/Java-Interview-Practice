38e1ebe73103f76cf2ba778f4e2136987c2c5839
quytr <quy.truong310@gmail.com>
2022-06-09 10:16:07 -0400

    replit 218 solved

--- src/com/syntax/question218/Main.java
+++ src/com/syntax/question218/Main.java
@@ -1,13 +1,11 @@
 package com.syntax.question218;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     /*
-    Count the number of repetations of words in an ArrayList append that count and return that list. You need to implent a function countDeviceNames that takes an ArrayList as an input and returns an ArrayList
+    Count the number of repetations of words in an ArrayList append that count and return that list. You need to implent a
+    function countDeviceNames that takes an ArrayList as an input and returns an ArrayList
 
 input ["switch","tv","switch","tv","switch","tv"]
 
@@ -32,41 +30,54 @@ input ["switch","tv"]
 output [switch, tv]
      */
 
-    // NOT DONE
 
     public static List<String> countDeviceNames(List<String> deviceNames) {
-        List<String> newList = new ArrayList<>();
-
-        Map<String, Integer> counts = new LinkedHashMap<>();
-        for (String device : deviceNames) {
-            if (counts.containsKey(device)) {
-                counts.put(device, counts.get(device) + 1);
-            } else {
-                counts.put(device, 1);
-            }
-        }
-//        System.out.println(counts);
-        for (Map.Entry<String, Integer> entry : counts.entrySet()) {
-//            System.out.println(entry);
-//            newList.add(entry.getKey());
-
-            for (int i = 0; i < entry.getValue(); i++) {
-                if (entry.getValue() > 1) {
-                    if(i==0){
-                        newList.add(entry.getKey());
-                    }else{
-                        newList.add(entry.getKey() + i);
-                    }
-                } else {
-                    newList.add(entry.getKey());
+        // create a arrat list to hold a unique name
+        List<String> uniqueNames = new ArrayList<>();
+        // for loop to go through the list to check duplicate
+        for (int i = 0; i < deviceNames.size(); i++) {
+            // create string variable to hold the name of given device name to check through the given array
+            String name = deviceNames.get(i);
+            // set duplicate is false, if duplicate is false, then we can add the name to unique array, if duplicate is true, continue to check
+            boolean duplicate = false;
+            // create another for loop, condition j < i to avoid array out of bond, to set duplicate is true if unique list contains variable name
+            for (int j = 0; j < i; j++) {
+                if (uniqueNames.get(j).equals(name)) {
+                    duplicate = true;
+                    break;
                 }
             }
+            // if duplicate is false, then simply add to the list.
+            if (!duplicate) {
+                uniqueNames.add(i, name);
+                // if duplicate is true, then continue to check
+            } else {
+                // first set id = 1 to give the duplicate a unique name, id = 1 as it continuously counts, id is unique
+                int id = 1;
+                // like duplicate, create a boolean exist to check even with new name with id, if it exists on the unique list or not
+                // first set it as true because we are on duplicate is true
+                boolean exist = true;
+                // create a while loop to check for the unique list again
+                while (exist) {
+                    // set exist to false to quit the loop
+                    exist = false;
+                    for (int j = 0; j < i; j++) {
+                        // if the unique list contains the combine (name + id), then id increase 1
+                        if (uniqueNames.get(j).equals(name + id)) {
+                            id++;
+                        }
+                    }
+                }
+                uniqueNames.add(i, (name + id));
+            }
         }
-        return newList;
+
+        return uniqueNames;
     }
 
+
     public static void main(String[] args) {
-        List<String> a = new ArrayList<>();
+        ArrayList<String> a = new ArrayList<>();
         a.add("switch");
         a.add("TV");
         a.add("switch");
@@ -76,7 +87,7 @@ output [switch, tv]
         System.out.println(countDeviceNames(a));
 
 
-        List<String> b = new ArrayList<>();
+        ArrayList<String> b = new ArrayList<>();
         b.add("switch");
         b.add("TV");
         b.add("switch");
@@ -85,12 +96,49 @@ output [switch, tv]
         System.out.println(countDeviceNames(b));
 
 
-        List<String> c = new ArrayList<>();
+        ArrayList<String> c = new ArrayList<>();
         c.add("switch");
         c.add("TV");
         System.out.println(countDeviceNames(c));
 
+//        String[] devices = {"switch", "tv", "switch", "tv", "switch", "tv", "radio", "tv"};
+//        System.out.println(deviceNamesSystem(Arrays.asList(devices)));
+
     }
+
+//    public static List<String> deviceNamesSystem(List<String> deviceNames) {
+//        String[] uniqueNames = new String[deviceNames.size()];
+//        for (int i = 0; i < deviceNames.size(); i++) {
+//            String name = deviceNames.get(i);
+//            boolean duplicate = false;
+//            for (int j = 0; j < i; j++) {
+//                if (uniqueNames[j].equals(name)) {
+//                    duplicate = true;
+//                    break;
+//                }
+//            }
+//            if (!duplicate) {
+//                uniqueNames[i] = name;
+//            } else {
+//                int id = 1;
+//                boolean exists = true;
+//                while (exists) {
+//                    exists = false;
+//                    for (int j = 0; j < i; j++) {
+//                        if (uniqueNames[j].equals(name + id)) {
+//                            exists = true;
+//                            id++;
+//                            break;
+//                        }
+//                    }
+//                }
+//                uniqueNames[i] = name + id;
+//            }
+//        }
+//        return Arrays.asList(uniqueNames);
+//    }
 }
+
+
 
 

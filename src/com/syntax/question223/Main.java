050a6d74192d37c7e313154c27357349e989e0a6
quytr <quy.truong310@gmail.com>
2022-06-12 19:34:08 -0400

    replits 223 done

--- src/com/syntax/question223/Main.java
+++ src/com/syntax/question223/Main.java
@@ -1,10 +1,14 @@
 package com.syntax.question223;
 
+import java.util.Scanner;
+
 public class Main {
     /*
-    Write logic in the main method to print a pyramid of numbers the logic should be generic i.e. it can print the pyrmid for any numbmer like 5, 6, 7 etc if input is 2 output should be
+    Write logic in the main method to print a pyramid of numbers the logic should be generic i.e.
+    it can print the pyrmid for any numbmer like 5, 6, 7 etc
+    if input is 2 output should be
 
-&nbsp 2
+2
 
 2 2
 
@@ -18,4 +22,43 @@ if input is 3 the output should be
 
 Ask user input for the number for which he wants to make the pyramid using scanner
      */
+
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Which number you want to make the pyramid? 1-9");
+//
+//        int numb = scanner.nextInt();
+//
+//        for (int i = 0; i < numb; i++) {
+//            for (int j = numb - i; j > 1; j--) {
+//
+//                System.out.print(" ");
+//            }
+//
+//            for (int j = 0; j <= i; j++) {
+//                System.out.print(numb + " ");
+//            }
+//            System.out.println();
+//
+//        }
+
+        int rows = 5, k = 0;
+
+        for (int i = 1; i <= rows; ++i, k = 0) {
+            for (int space = 1; space <= rows - i; ++space) {
+                System.out.print("  ");
+            }
+
+            while (k != 2 * i - 1) {
+                System.out.print("* ");
+                ++k;
+            }
+
+            System.out.println();
+        }
+
+
+    }
 }
+

3b658d3b28c22deaf356a12e6527660ded8bdbca
quytr <quy.truong310@gmail.com>
2022-06-13 00:30:07 -0400

    replits 228 done

--- src/com/syntax/question228/Main.java
+++ src/com/syntax/question228/Main.java
@@ -8,6 +8,31 @@ public class Main {
 
 "i love coding" =>6
 
-"sun" =>3 ""=>0
+"sun" =>3
+
+""=>0
      */
+
+    public static int countLastWordChar(String str) {
+
+        String[] arrStr = str.split(" ");
+        return arrStr[arrStr.length - 1].length();
+
+    }
+
+
+    public static void main(String[] args) {
+        String a = "I love Java";
+        System.out.println(countLastWordChar(a));
+
+        String b = "I love coding";
+        System.out.println(countLastWordChar(b));
+
+        String c = "sun";
+        System.out.println(countLastWordChar(c));
+
+        String d = "";
+        System.out.println(countLastWordChar(d));
+
+    }
 }

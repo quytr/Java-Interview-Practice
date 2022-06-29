a9941793592be68a798d10f024c69cd727160586
quytr <quy.truong310@gmail.com>
2022-06-29 17:36:47 -0400

    finally 229 is DONE

--- src/com/syntax/question229/MANDATORY.java
+++ src/com/syntax/question229/MANDATORY.java
@@ -1,6 +1,9 @@
 package com.syntax.question229;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MANDATORY {
     /*
     Count the maximum repetition of a character examples are given below
@@ -18,71 +21,52 @@ public class MANDATORY {
 
     public static int countMaxChar(String str) {
 
-        char[] arrStr = str.toCharArray();
+        char[] arrCh = str.toCharArray();    // create a character array to store individual character of given string and convert the string to array.
 
-        int max_count = 0;
-        int count = 1;
-        int size = str.length();
+        int count = 1;           // to count to repetition of a character, the count = 1 because we start from 1 (we count the character itself)
+        int maxCount = 0;        // to compare if there are more than 1 different character is repeated.
+        int size = arrCh.length; // the size of the array
 
+        // the first for loop will hold the first character and start comparing to the second character and so on the inner loop
+        // keep iterating it
         for (int i = 0; i < size; i++) {
-            for (int j = 1; j < size; j++) {
-                if (arrStr[i] == arrStr[j]) {
+            // set j = i + 1 so we dont have to recheck the previous character
+            for (int j = i + 1; j < size; j++) {
+                // when we find the character is repeated, the count will be increment by 1
+                if (arrCh[i] == arrCh[j]) {
                     count++;
-                } else {
-                    if (count > max_count) {
-                        max_count = count;
-                    }
-                    count = 1;
                 }
             }
-        }
-        if (count > max_count) {
-            max_count = count;
-        }
+            // update the max count, if we find another character is repeated and it has more count than the previous one
+            // then max count will be updated
+            if (maxCount < count) {
+                maxCount = count;
+            }
+            // the count has to reset back for the next character
+            count = 1;
 
-        return max_count;
-
+        }
+        // the maximum repetition of a character
+        return maxCount;
 
     }
 
     public static void main(String[] args) {
-        
 
-//        String a = "abscaaaaabdddd";
-//        System.out.println(countMaxChar(a));
-//
-//        String b = "dddddddddd";
-//        System.out.println(countMaxChar(b));
-//
-//        String c = "daccccdndeeeennnnndddddd";
-//        System.out.println(countMaxChar(c));
-//
-//        String d = "a";
-//        System.out.println(countMaxChar(d));
+        String a = "abscaaaaabdddd";
+        System.out.println(countMaxChar(a));
+
+        String b = "dddddddddd";
+        System.out.println(countMaxChar(b));
+
+        String c = "daccccdndeeeennnnndddddd";
+        System.out.println(countMaxChar(c));
+
+        String d = "a";
+        System.out.println(countMaxChar(d));
 
         String e = "";
         System.out.println(countMaxChar(e));
     }
 }
-/*
-char[] array = S.toCharArray()
-int count = 1;
-int max = 0;
-char maxChar = 0;
-for(int i=1; i<array.length; i++){ // Start from 1 since we want to compare it with the char in index 0
-    if(array[i]==array[i-1]){
-        count++;
-    } else {
-        if(count>max){  // Record current run length, is it the maximum?
-            max=count;
-            maxChar=array[i-1];
-        }
-        count = 1; // Reset the count
-    }
-}
-if(count>max){
-    max=count; // This is to account for the last run
-    maxChar=array[array.length-1];
-}
-System.out.println("Longest run: "+max+", for the character "+maxChar); // Print the maximum.
- */
+
